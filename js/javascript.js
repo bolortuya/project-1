b308961d327062b230c02c7685ed67358175f446
bolortuya tumurbaatar <btumurbaatar@hawk.iit.edu>
2019-10-03 23:57:10 -0500

    add event listner

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var Onclick = document.getElementById("btn");
+    Onclick.addEventListener("click", function() {
+        alert ("Your discount is sent!" + alertInfo.value);
+    });
+}
